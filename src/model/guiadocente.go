f39355b034c67b2b09f5df36ccdb7eb129a59d75
Mario Piña Munera <73356942+Mario25402@users.noreply.github.com>
2024-11-22 13:57:09 +0100

    Borra código antiguo de #9.

--- src/model/guiadocente.go
+++ src/model/guiadocente.go
@@ -13,20 +13,6 @@ type GuiaDocente struct{
 	BibliografiaComplementaria []Libro  // Lista de libros complementarios
 }
 
-
-// Crea una nueva guía docente con datos válidos
-func NuevaGuiaDocente(anio int, asignatura string, basica []Libro, complementaria []Libro) (*GuiaDocente, error){
-	anioValido := EsPositivo(anio)
-
-	// Ambos datos inválidos
-	if !anioValido {
-		return nil, errors.New("El año debe ser positivo")
-	}
-
-	// Datos válidos: se crea la guía docente sin devolver errores
-	return &GuiaDocente{anio, asignatura, basica, complementaria}, nil
-}
-
 // NuevaGuiaDocente crea una nueva guía docente a partir de un handler de fichero.
 // El handler debe estar previamente abierto para lectura y contendrá los datos necesarios 
 // para inicializar la estructura. El contenido del fichero será procesado en el futuro 
